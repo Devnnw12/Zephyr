8986c16062e8420974a2bea1cabb7e9518d72160
Devennw <dvennote65@gmail.com>
2026-06-25 19:55:53 +0700

    update

---
+++ boot/arm64/include/asm/boot.h
@@ -0,0 +1,11 @@
+#ifendef __ASM_BOOT_H
+#define __ASM_BOOT_H
+
+//isi include dari ekstensi header .hpp
+
+#define MIN_FTD_ALIGN   8
+#define MAX_FTD_SIZE    SZ_2M
+
+#define MIN_KIMG_ALIGN  SZ_2M
+
+#endif
