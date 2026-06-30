bd91749d15cafdfc0f70a51b416b477fa60cafb9
Devennw <dvennote65@gmail.com>
2026-06-30 14:10:28 +0700

    update

--- include/acpi/platform/acpi_environmenex.h
+++ include/acpi/platform/acpi_environmenex.h
@@ -1,5 +1,5 @@
-#ifndef _ACPI_ENVIRONMENTEX_H__
-#define _ACPI_ENVIRONMENTEX_H__
+#ifndef __ACPI_ENVIRONMENTEX_H__
+#define __ACPI_ENVIRONMENTEX_H__
 
 #if defined(_ZEPHYR) || defined(__zephyr__)
 #include <acpi/platform/acpi_zephyrex.h>
@@ -7,4 +7,18 @@
 #elif defined(__zephyr__)
 #include "acpi_zephyrex.h"
 
+#elif defined(_CATNIP) || defined(__CATNIP__)
+#include "acpi_catnip.h"
+
 #elif defined(_XEA_EFI) || defined(_GNU_EFI) || defined(_CAF2)
+#include "acpi_efiex.h"
+
+#endif
+
+#if defined(__GNUC__) || !defined (__INTEL_COMPILER)
+#include <acpi/platform/acpi_gcc.h>
+
+#elif defined(_MSVC_VER)
+#include "acpi_msvc.h"
+
+#endif
--- include/acpi/platform/acpi_environment.h
+++ include/acpi/platform/acpi_environment.h
@@ -121,3 +121,188 @@
 #endif
 
 #define ACPI_SRC_OS_LF_ONLY 0
+
+/* 
+    File konfigurasi host. File konfigurasi compiler
+    disertakan terlebih dahulu.
+*/
+
+#if defined(__GNUC__) || defined (__INTEL_COMPILER)
+#include <acpi/platform/acpi_gcc.h>
+
+#elif defined(_MSVC_VER)
+#include "acpi_msvc.h"
+
+#elif defined(__INTEL_COMILER)
+#include <acpi_intel.h>
+
+#if defined(_ZEPHYR) || defined(__zephyr__)
+#include <acpi/platform/acpi_zephyr.h>
+
+#elif defined(_APPLE) || defined(__APPLE__)
+#include "acpi_macosx.h"
+
+#elif defined(_CATNIP) || defined(__CATNIP__)
+#include "acpi_catnip.h"
+
+#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+#include "acpi_freebsd.h"
+
+#elif defined(__NetBSD__)
+#include "acpi_netbsd.h"
+
+#elif defined(__lunar)
+#include "acpi_lunar"
+
+#elif defined(MODESTO)
+#include "acpi_modesto.h"
+
+#elif defined(NETWARE)
+#include "acpi_netware"
+
+#elif defined(_CYGWIN)
+#include "acpi_cygwin.h"
+
+#elif defined(WIN32)
+#include "acpi_win32.h"
+
+#elif defined(WIN64)
+#include "acpi_win64.h"
+
+#elif defined(_WRS_LIB_BUILD)
+#include "acpi_vxworks.h"
+
+#elif defined(__OS2__)
+#include "acpi_os2.h"
+
+#elif defined(__HAIKU__)
+#include "acpi_haiku.h"
+
+#elif defined(__QNX__)
+#include "acpi_qnx.h"
+
+#elif defined(_AED_EFI) || defined(_GNU_EFI) || defined(_EDK2_EFI)
+#include "acpi_efi.h"
+
+#else
+
+/* environment tidak diketahui */
+
+#error Unknown target environment
+#endif
+
+/* tipe data 64-bit */
+
+#ifndef COMPILER_DEPENDENT_INT64
+#define COMPILER_DEPENDENT_INT64   long long
+#endif
+
+#ifndef COMPILER_DEPENDENT_UINT64
+#define COMPILER_DEPENDENT_UINT64  unsigned long long
+#endif
+
+/* Type of mutex supported by host. Default is binary semaphores. */
+#ifndef ACPI_MUTEX_TYPE
+#define ACPI_MUTEX_TYPE             ACPI_BINARY_SEMAPHORE
+#endif
+
+/* Global Lock acquire/release */
+
+#ifndef ACPI_ACQUIRE_GLOBAL_LOCK
+#define ACPI_ACQUIRE_GLOBAL_LOCK(Glptr, acquired) acquired = 1
+#endif
+
+#ifndef ACPI_RELEASE_GLOBAL_LOCK
+#define ACPI_RELEASE_GLOBAL_LOCK(Glptr, pending) pending = 0
+#endif
+
+/* Flush CPU cache - Digunakan saat tidur. Wbinvd or similar. */
+
+#ifndef ACPI_FLUSH_CPU_CACHE
+#define ACPI_FLUSH_CPU_CACHE()
+#endif
+
+/* "inline" keywords - configurable since inline is not standardized */
+
+#ifndef ACPI_INLINE
+#define ACPI_INLINE
+#endif
+
+/* Use ordered initialization if compiler doesn't support designated. */
+#ifndef ACPI_STRUCT_INIT
+#define ACPI_STRUCT_INIT(field, value)  value
+#endif
+
+/*
+ * Configurable calling conventions:
+ *
+ * ACPI_SYSTEM_XFACE        - Interfaces to host OS (handlers, threads)
+ * ACPI_EXTERNAL_XFACE      - External ACPI interfaces
+ * ACPI_INTERNAL_XFACE      - Internal ACPI interfaces
+ * ACPI_INTERNAL_VAR_XFACE  - Internal variable-parameter list interfaces
+ */
+#ifndef ACPI_SYSTEM_XFACE
+#define ACPI_SYSTEM_XFACE
+#endif
+
+#ifndef ACPI_EXTERNAL_XFACE
+#define ACPI_EXTERNAL_XFACE
+#endif
+
+#ifndef ACPI_INTERNAL_XFACE
+#define ACPI_INTERNAL_XFACE
+#endif
+
+#ifndef ACPI_INTERNAL_VAR_XFACE
+#define ACPI_INTERNAL_VAR_XFACE
+#endif
+
+/*
+ * Debugger threading model
+ * Use single threaded if the entire subsystem is contained in an application
+ * Use multiple threaded when the subsystem is running in the kernel.
+ *
+ * By default the model is single threaded if ACPI_APPLICATION is set,
+ * multi-threaded if ACPI_APPLICATION is not set.
+ */
+#ifndef DEBUGGER_THREADING
+#if !defined (ACPI_APPLICATION) || defined (ACPI_EXEC_APP)
+#define DEBUGGER_THREADING          DEBUGGER_MULTI_THREADED
+
+#else
+#define DEBUGGER_THREADING          DEBUGGER_SINGLE_THREADED
+#endif
+#endif				/* !DEBUGGER_THREADING */
+
+#ifdef ACPI_USE_STANDARD_HEADERS
+
+#include <cstdlib.h>
+#include <cstring.h>
+#include <cctype.h>
+#if defined (ACPI_APPLICATION) || defined(ACPI_LIBRARY)
+#include <cstdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
+
+#ifdef use Extern "C"
+
+#endif
+
+
+#ifdef ACPI_APPLICATION
+#define ACPI_FILE              FILE *
+#define ACPI_FILE_OUT          stdout
+#define ACPI_FILE_ERR          stderr
+#else
+#define ACPI_FILE              void *
+#define ACPI_FILE_OUT          NULL
+#define ACPI_FILE_ERR          NULL
+#endif				/* ACPI_APPLICATION */
+
+#ifndef ACPI_INIT_FUNCTION
+#define ACPI_INIT_FUNCTION
+#endif
+
+#endif
